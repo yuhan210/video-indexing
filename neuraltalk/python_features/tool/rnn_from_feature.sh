fa3f722707d9c3f67945eb00f99c71c39d65f449
Tiffany <yuhan210@gmail.com>
2015-06-16 11:55:31 -0700

    add CNN batch testing script

---
+++ neuraltalk/python_features/tool/rnn_from_feature.sh
@@ -0,0 +1,20 @@
+python /home/yuhan/neuraltalk/predict_on_images.py /home/yuhan/neuraltalk/models/flickr8k_cnn_lstm_v1.p -r /var/www/msr/Hercules_playing_on_the_beach -t /home/yuhan/neuraltalk/python_features/tool/Hercules_playing_on_the_beach_tasks.txt -f Hercules_playing_on_the_beach.pickle -of /var/www/msr/Hercules_playing_on_the_beach_1_caption.json
+
+python /home/yuhan/neuraltalk/predict_on_images.py /home/yuhan/neuraltalk/models/flickr8k_cnn_lstm_v1.p -r /var/www/msr/Polar_bears_and_dogs_playing -t /home/yuhan/neuraltalk/python_features/tool/Polar_bears_and_dogs_playing_tasks.txt -f Polar_bears_and_dogs_playing.pickle -of /var/www/msr/Polar_bears_and_dogs_playing_1_caption.json
+
+python /home/yuhan/neuraltalk/predict_on_images.py /home/yuhan/neuraltalk/models/flickr8k_cnn_lstm_v1.p -r /var/www/msr/KIDS_PLAYING_BASKETBALL_2 -t /home/yuhan/neuraltalk/python_features/tool/KIDS_PLAYING_BASKETBALL_2_tasks.txt -f KIDS_PLAYING_BASKETBALL_2.pickle -of /var/www/msr/KIDS_PLAYING_BASKETBALL_2_1_caption.json
+
+python /home/yuhan/neuraltalk/predict_on_images.py /home/yuhan/neuraltalk/models/flickr8k_cnn_lstm_v1.p -r /var/www/msr/GoPro-_Airplane_Waterskiing -t /home/yuhan/neuraltalk/python_features/tool/GoPro-_Airplane_Waterskiing_tasks.txt -f GoPro-_Airplane_Waterskiing.pickle -of /var/www/msr/GoPro-_Airplane_Waterskiing_1_caption.json
+
+python /home/yuhan/neuraltalk/predict_on_images.py /home/yuhan/neuraltalk/models/flickr8k_cnn_lstm_v1.p -r /var/www/msr/12_year_old_Catcher_celebrates_a_strikeout_and_a_win -t /home/yuhan/neuraltalk/python_features/tool/12_year_old_Catcher_celebrates_a_strikeout_and_a_win_tasks.txt -f 12_year_old_Catcher_celebrates_a_strikeout_and_a_win.pickle -of /var/www/msr/12_year_old_Catcher_celebrates_a_strikeout_and_a_win_1_caption.json
+
+python /home/yuhan/neuraltalk/predict_on_images.py /home/yuhan/neuraltalk/models/flickr8k_cnn_lstm_v1.p -r /var/www/msr/Funny_Dogs_Playing_Fetch_In_Sea_At_Goa_Beach_-_Pets_and_Animals_Video -t /home/yuhan/neuraltalk/python_features/tool/Funny_Dogs_Playing_Fetch_In_Sea_At_Goa_Beach_-_Pets_and_Animals_Video_tasks.txt -f Funny_Dogs_Playing_Fetch_In_Sea_At_Goa_Beach_-_Pets_and_Animals_Video.pickle -of /var/www/msr/Funny_Dogs_Playing_Fetch_In_Sea_At_Goa_Beach_-_Pets_and_Animals_Video_1_caption.json
+
+python /home/yuhan/neuraltalk/predict_on_images.py /home/yuhan/neuraltalk/models/flickr8k_cnn_lstm_v1.p -r /var/www/msr/kids_playing_basketball -t /home/yuhan/neuraltalk/python_features/tool/kids_playing_basketball_tasks.txt -f kids_playing_basketball.pickle -of /var/www/msr/kids_playing_basketball_1_caption.json
+
+python /home/yuhan/neuraltalk/predict_on_images.py /home/yuhan/neuraltalk/models/flickr8k_cnn_lstm_v1.p -r /var/www/msr/Periscope-_upload_Periscope_video_to_YouTube -t /home/yuhan/neuraltalk/python_features/tool/Periscope-_upload_Periscope_video_to_YouTube_tasks.txt -f Periscope-_upload_Periscope_video_to_YouTube.pickle -of /var/www/msr/Periscope-_upload_Periscope_video_to_YouTube_1_caption.json
+
+python /home/yuhan/neuraltalk/predict_on_images.py /home/yuhan/neuraltalk/models/flickr8k_cnn_lstm_v1.p -r /var/www/msr/Cat_and_Dog_Play_Fighting -t /home/yuhan/neuraltalk/python_features/tool/Cat_and_Dog_Play_Fighting_tasks.txt -f Cat_and_Dog_Play_Fighting.pickle -of /var/www/msr/Cat_and_Dog_Play_Fighting_1_caption.json
+
+python /home/yuhan/neuraltalk/predict_on_images.py /home/yuhan/neuraltalk/models/flickr8k_cnn_lstm_v1.p -r /var/www/msr/Newborn_Goat_tries_to_cross_over_Dog -t /home/yuhan/neuraltalk/python_features/tool/Newborn_Goat_tries_to_cross_over_Dog_tasks.txt -f Newborn_Goat_tries_to_cross_over_Dog.pickle -of /var/www/msr/Newborn_Goat_tries_to_cross_over_Dog_1_caption.json
+
