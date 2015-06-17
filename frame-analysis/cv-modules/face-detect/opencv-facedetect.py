99539ff739e6b7ed026a6d82b77250c84d1cebf8
Tiffany <yuhan210@gmail.com>
2015-06-17 14:25:24 -0700

    face detect cv module

---
+++ frame-analysis/cv-modules/face-detect/opencv-facedetect.py
@@ -0,0 +1,48 @@
+#!/usr/bin python
+
+import numpy as np
+import cv2
+
+# local modules
+from video import create_capture
+from common import clock, draw_str
+
+
+def detect(img, cascade):
+    rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(25, 25), flags = cv2.CASCADE_SCALE_IMAGE)
+    if len(rects) == 0:
+        return []
+    rects[:,2:] += rects[:,:2]
+    return rects
+
+def draw_rects(img, rects, color):
+    for x1, y1, x2, y2 in rects:
+        cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
+
+if __name__ == '__main__':
+
+    import sys, getopt
+
+    img_name = sys.argv[1]
+    
+    cascade_fn = "./models/haarcascades/haarcascade_frontalface_alt.xml"
+    cascade = cv2.CascadeClassifier(cascade_fn)
+
+    img = cv2.imread(img_name)
+
+    while True:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.equalizeHist(gray)
+
+        t = clock()
+        rects = detect(gray, cascade)
+        vis = img.copy()
+        draw_rects(vis, rects, (0, 255, 0))
+        dt = clock() - t
+
+        draw_str(vis, (20, 20), 'time: %.1f ms' % (dt*1000))
+        cv2.imshow('facedetect', vis)
+
+        if 0xFF & cv2.waitKey(5) == 27:
+            break
+    cv2.destroyAllWindows()
