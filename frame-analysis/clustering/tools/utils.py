7bce14dd98b4b2fd09b183cb10cb3fed37706dd3
Tiffany <yuhan210@gmail.com>
2015-06-19 11:53:10 -0700

    read cv modules

--- frame-analysis/clustering/tools/utils.py
+++ frame-analysis/clustering/tools/utils.py
@@ -1,5 +1,101 @@
-import os
+from nltk.stem.wordnet import WordNetLemmatizer
 import json
+import nltk
+import os
+
+def getVerbFromStr(sentence):
+
+    segs = nltk.word_tokenize(sentence)
+    tags = nltk.pos_tag(segs)
+    
+    verbs = []
+    for word, tag in tags:
+        if tag == 'VB' or tag == 'VBZ' or tag == 'VBN' or tag == 'VBD' or tag == 'VBG':
+            verb_simple_tence = str(WordNetLemmatizer().lemmatize(word, 'v'))
+            
+            ## This is a hack. Remove 'be' verb  
+            if verb_simple_tence != 'be': 
+                verbs += [verb_simple_tence]
+
+    return verbs
+
+
+def getVerbTfFromCaps(captions, method = 'equal'):
+    #TODO: implement weighted version
+  
+    verb_tf = {}
+    for sentence in captions:
+
+        verbs = getVerbFromStr(sentence)
+        for verb in verbs:
+            if verb not in verb_tf:
+                verb_tf[verb] = 1
+            else:
+                verb_tf[verb] += 1        
+
+    return verb_tf 
+
+
+def load_video_peopledet(peopled_folder, video_name):
+
+    file_pref = os.path.join(peopled_folder, video_name)
+
+    # load recognition
+    with open(file_pref + '_openpd.json') as json_file:
+        pd_data = json.load(json_file)
+
+    pd_data = sorted(pd_data['img_blobs'], key=lambda x: int(x['img_name'].split('.')[0]))
+    
+    return pd_data
+
+
+def load_video_ocr(ocr_folder, video_name):
+    file_pref = os.path.join(ocr_folder, video_name)
+    
+    # load recognition
+    with open(file_pref + '_ocr.json') as json_file:
+        ocr_data = json.load(json_file)
+
+    ocr_data = sorted(ocr_data['img_blobs'], key=lambda x: int(x['img_name'].split('.')[0]))
+    
+    return ocr_data
+
+def load_video_dlibfd(dlibfd_folder, video_name):
+
+    file_pref = os.path.join(dlibfd_folder, video_name)
+    
+    # load face detection
+    with open(file_pref + '_dlibfd.json') as json_file:
+        faced_data = json.load(json_file)
+
+    faced_data = sorted(faced_data['img_blobs'], key=lambda x: int(x['img_name'].split('.')[0]))
+    
+    return faced_data
+
+def load_video_rcnn(rcnn_folder, video_name):
+    
+    file_pref = os.path.join(rcnn_folder, video_name)
+    
+    # load face detection
+    with open(file_pref + '_rcnnrecog.json') as json_file:
+        rcnn_data = json.load(json_file)
+
+    rcnn_data = sorted(rcnn_data['imgblobs'], key=lambda x: int(x['image_path'].split('/')[-1].split('.')[0]))
+    
+    return rcnn_data
+
+    
+
+def load_video_opencvfd(opencvfd_folder, video_name):
+    file_pref = os.path.join(opencvfd_folder, video_name)
+    
+    # load face detection
+    with open(file_pref + '_openfd.json') as json_file:
+        faced_data = json.load(json_file)
+
+    faced_data = sorted(faced_data['img_blobs'], key=lambda x: int(x['img_name'].split('.')[0]))
+    
+    return faced_data
 
 def load_video_recog(recog_folder, video_name):
     
--- frame-analysis/clustering/tools/wordnet.py
+++ frame-analysis/clustering/tools/wordnet.py
@@ -2,22 +2,53 @@ from nltk.corpus import wordnet
 
 import pandas as pd
 import sys
-
+import json
+import os
 
 def getHypernyms(words):
      
     words = [ '_'.join(word.strip().split(' ')) for word in words]
-    print words
 
     synset_list = []    
     for word in words:
         synset_list += [wordnet.synsets(word)]
                
-    syn = list(set.intersection(*map(set, synset_list)))[0]
+    syns = list(set.intersection(*map(set, synset_list)))
+
+    best_words = words
     
-    print syn.hypernyms()
+    ## pick one
+   
+     
+    print best_words
+    while(True):
+ 
+        ''' printing '''
+        for idx, syn in enumerate(syns):
+            print idx, syn, syn.definition()
+        print ''
+   
+        ''' picking ''' 
+        syn_ind = raw_input('pick one syn (0)')
+        if len(syn_ind) == 0:
+            syn_ind = 0
 
-
+        syn = syns[int(syn_ind)]
+        print 'pick syn:', syn, syn.lemma_names()
+        print ''
+        ## whether go up
+        ''' picking '''
+        go_up_str = raw_input('go up? (y/(n))')       
+        if len(go_up_str) == 0 or go_up_str == 'y':
+            syns = syn.hypernyms()
+        else:
+            best_words = syn.lemma_names()
+            print  words, ' to ' , best_words , ' '
+            
+            break
+    
+    
+    return best_words
 def preprocessSynsetWords(file_name):
     
     with open(file_name) as f:
@@ -29,21 +60,32 @@ def preprocessSynsetWords(file_name):
         }
         for l in f.readlines()
     ])
-    
     labels = labels_df.sort('synset_id')['name'].values
 
-
-    
     label_match = {}
-    for label in labels:
-         
-        getHypernyms(label)
-        
+     
+    # load things labels from json file
+    
+    save_file = 'new_synset_word.txt'
+    new_labels = {}
+    if os.path.exists(save_file):
+        new_labels = json.load(open(save_file))
+ 
+    for idx, label in enumerate(labels):
+        key = '_'.join(label)
+        if key not in new_labels: 
+            new_label = getHypernyms(label)
+            print label, new_label 
+            new_labels[key] = new_label
+        # dump result to json file
+        json.dump(new_labels, open(save_file, 'w'))
+  
     #print label_match
 
 
 if __name__ == "__main__":
     
     synset_file = "/home/t-yuche/caffe/data/ilsvrc12/synset_words.txt"
+    
     preprocessSynsetWords(synset_file)
     # dump in json
