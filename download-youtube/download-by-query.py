0da6d60332eacde0b8d0a128c95d00d5af4b87d8
Tiffany <yuhan210@gmail.com>
2015-06-25 17:21:05 -0700

    only download videos with creativecommons

--- download-youtube/download-by-query.py
+++ download-youtube/download-by-query.py
@@ -1,17 +1,16 @@
 from bs4 import BeautifulSoup 
 from common import createVideoMeatadata, getNewVideoName, downloadVideo
+import socket
 import urllib2
+import urllib
 import pafy
 import sys
 import json
 import re
 
+def parseWebpage(query_url):
 
-def getWatchUrls(query):
-
-    query_url = "https://www.youtube.com/results?search_query=" + query
     watch_urls = []
-
     try:
         html_doc = urllib2.urlopen(query_url).readlines()
 
@@ -27,25 +26,37 @@ def getWatchUrls(query):
 
     for line in html_doc:
         if line.find('</div><div class="yt-lockup-content">') >= 0:
-            watch_url = line.split('a href')[1].split('class=')[0].split('"')[1]
+            watch_url = line.split('a href')[1].split('class=')[0].split('"')[1].split('v=')[1]
             watch_urls += [watch_url]
-            
+        
+     
     return watch_urls
  
 
+
+def getWatchUrls(query, max_number=50):
+
+    # https://www.youtube.com/results?filters=creativecommons&search_query=dog+playing&page=1
+    query_prefix = "https://www.youtube.com/results?search_query=" + urllib.quote_plus(query) + "&filters=creativecommons&page="
+    watch_urls = []
+
+    page_number = 1
+    while len(watch_urls) < max_number:
+        
+        query_url = query_prefix + str(page_number)
+        page_urls = parseWebpage(query_url)
+        watch_urls.extend(page_urls)
+        print page_number, page_urls, len(watch_urls)
+        page_number += 1
+    
+    return watch_urls[:max_number]
+    
 if __name__ == "__main__":
 
     input_str = (raw_input('$ '))
-    query = '+'.join(input_str.split(' '))
 
-    #https://www.youtube.com/results?search_query=basketball        
-    watch_urls = getWatchUrls(query)
+    watch_urls = getWatchUrls(input_str, 1)
     
-    # get top-10 urls
-    watch_urls = [0: min(10, len(watch_urls))]
-
     for watch_url in watch_urls:
         video = pafy.new(watch_url) 
         downloadVideo(video) 
-        break
-            
