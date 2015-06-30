781c8713a12d10c27c6ced5a7346c77bcad2b229
Tiffany <yuhan210@gmail.com>
2015-06-30 15:14:24 -0700

    gen batch script chcked in

--- gt-labeling/frame-subsample/gen-batch-script.py
+++ gt-labeling/frame-subsample/gen-batch-script.py
@@ -4,5 +4,5 @@ import os
 
 for f in os.listdir(sys.argv[1]):
     
-    print 'python subsample.py ' + os.path.join(sys.argv[1], f)
+    print 'python subsample.py ' + os.path.join(sys.argv[1], f) + ' &'
 
