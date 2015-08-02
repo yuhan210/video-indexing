e48f1d7c8c06e5f8d07482bfcd19df27caa657e7
Tiffany <yuhan210@gmail.com>
2015-08-02 00:16:08 -0700

    write captioning results into file

--- msr-captioning/vision_concepts_linux64/batch_msr_captioning.py
+++ msr-captioning/vision_concepts_linux64/batch_msr_captioning.py
@@ -4,6 +4,8 @@ import _init_paths #sets up paths with caffe and test_utils
 import cPickle
 import demo_test_utils as tutils 
 import sys
+import time
+import json
 
 def loadModel(prototxt_file, model_file, vocab_file):
     means = np.array([[[ 103.939, 116.779, 123.68]]]);
@@ -41,15 +43,19 @@ def printWordsWithProb(mil_prob, model, removeFunctional = False):
             print mil_prob[i], words[i];
         
 
-def main():
+if __name__ == "__main__":
 
-    if len(sys.argv) != 2:
-        print 'Usage:', sys.argv[0], ' task_file'
+    if len(sys.argv) != 4:
+        print 'Usage:', sys.argv[0], 'task_file out_file'
         exit(-1)
 
     task_file = sys.argv[1]
+    out_file = sys.argv[2]
     img_names = open(task_file, 'r').read().splitlines()
-    top_k = 5
+    top_k = 60
+    blob = {}
+    blob['imgblobs'] = []
+    print img_names
 
     prototxt_file = 'demoData/mil_finetune.prototxt.deploy';
     model_file = 'demoData/snapshot_iter_240000.caffemodel';
@@ -58,11 +64,29 @@ def main():
 
     for imName in img_names:
         print 'testing ',imName
+        tic = time.time()
         mil_prob, sc = testImg(imName, model)
+        toc = time.time()
+        print toc - tic
+        srt_inds = np.argsort(sc)[::-1]; #sort in descending order
+        words = model['vocab']['words'];
+        print '%s: '%(imName)
+        
+        texts = []
+        probs  = []  
+        for i in range(top_k):
+            texts += [words[srt_inds[i]]]
+            probs += [sc[srt_inds[i]]]
+        
+        img_blob = {}
+        # build up the output
+        img_blob = {}
+        img_blob['img_path'] = imName
+        img_blob['caption_time'] = (toc-tic)
+        img_blob['words'] = {'text': texts, 'prob':probs }
+        blob['imgblobs'] += img_blob
 
-	    srt_inds = np.argsort(sc)[::-1]; #sort in descending order
-    	words = model['vocab']['words'];
-    	print '%s: '%(imName)
-    	for i in range(top_k):
-	    	print '{:s} ({:.2f}), '.format(words[srt_inds[i]], sc[srt_inds[i]]);
+    print 'writing predictions to %s...' % (out_file, )
+    json.dump(blob, open(out_file, 'w'))
+
 
