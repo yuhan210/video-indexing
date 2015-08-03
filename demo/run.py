5b944758c0dcf6f1329a05abe8ef3b765a5f8899
Tiffany <yuhan210@gmail.com>
2015-08-02 17:12:15 -0700

    demo v1 is working

--- demo/run.py
+++ demo/run.py
@@ -1,5 +1,6 @@
 import threading
 import Queue
+import pickle
 import time
 import os
 import sys
@@ -12,6 +13,86 @@ VIDEO_FOLDER = "/home/t-yuche/deep-video/data/videos"
 
 event_queue = []
 
+class RankedVideoPlayer(threading.Thread):
+
+    def __init__(self, threadID, ranked_videos, stop_event):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        # (video_name, best_node, score)
+        self.ranked_videos = ranked_videos
+        self.stop_event = stop_event
+
+    def stop(self, caps):
+        
+        for cap in caps:
+            cap.release()
+
+        cv2.destroyAllWindows()
+        return
+
+    def init_videos(self, video_names, ranked_videos):
+        caps = []
+        video_chunks = []
+        for idx, video in enumerate(video_names):
+
+            if video.find('.mp4') < 0:
+                video += '.mp4'
+
+            video_path = os.path.join(VIDEO_FOLDER, video)
+            cap = cv2.VideoCapture(video_path)
+            caps.append(cap)
+
+            video_chunks += [(max(0, min(ranked_videos[idx][1]['n_idx'])), min(int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT)), max(ranked_videos[idx][1]['n_idx'])))]
+
+        return caps, video_chunks
+
+    def run(self):
+        COLUMN_NUM = 5
+
+        video_names = [x[0] for x in self.ranked_videos]
+        caps, video_chunks = self.init_videos(video_names, self.ranked_videos) 
+   
+        #create windows
+        for idx, video_name in enumerate(video_names):
+            cv2.namedWindow(video_name)
+            cv2.resizeWindow(video_name, 50, 60)
+            col = idx % 5
+            row = idx / 5 
+            cv2.moveWindow(video_name, col * 400, row * 400)
+         
+
+        #check if videos are opened - should always work
+        for idx, cap in enumerate(caps):
+            if (not cap.isOpened()):
+                print video_names[idx], 'not opened'
+                cv2.destroyAllWindows() 
+                return
+            else:
+                cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, video_chunks[idx][0])
+
+
+        #play all videos
+        frame_counters = [x[0] for x in video_chunks]
+        while(not self.stop_event.is_set()):
+
+            for idx, cap in enumerate(caps):
+                video_name = video_names[idx] 
+                
+                if frame_counters[idx] == video_chunks[idx][1]: # need to replay
+                    frame_counters[idx] = video_chunks[idx][0]
+                    cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, video_chunks[idx][0])
+               
+                  
+                ret, frame = cap.read()
+                frame = cv2.resize(frame, (350, 300))
+                cv2.imshow(video_name, frame)                
+                frame_counters[idx] += 1
+
+            c = cv2.waitKey(33)
+  
+        self.stop(caps) 
+ 
+    
 class VideosPlayerThread(threading.Thread):
 
     def __init__(self, threadID, video_names, stop_event):
@@ -103,45 +184,67 @@ class VideosPlayerThread(threading.Thread):
 
 
 def rank_video(matched_list):
-    
-    ranked_list = sorted(matched_list, key = lambda x:x[2], reverse=True) 
+     
+    ranked_list = sorted(matched_list, key = lambda x:x[2], reverse=True)
+    ranked_list = filter(lambda x: x[2] > 0, ranked_list) 
+
     return ranked_list
+
+
+#def play_video():
+
 
 if __name__ == "__main__":
 
-    if (len(sys.argv) != 2):
-        print 'Usage:', sys.argv[0], 'video_names'
+    if (len(sys.argv) != 3):
+        print 'Usage:', sys.argv[0], 'video_names index_file'
         exit(-1)
     
     video_names = [line.strip() for line in open(sys.argv[1]).readlines()]
-    #thread_lock = threading.Lock()
+    index_file = sys.argv[2]
     
-    # start creating index
+    # load index from index.pickle file
+    #indexes = {}
+    #with open(index_file, 'rb') as pickle_fh:
+    #   indexed = pickle.load(pickle_fh
     indexes = {}
     for idx, video_name in enumerate(video_names): 
-        if idx == 2:
+        if idx == 1:
             break
         video_name = video_name.split('.')[0]
         gt_nodes = load_turker_labels(video_name)
         clusters, linkage_matrix = cluster(gt_nodes)
         indexes[video_name] = clusters
-
-    thread_stop_event = threading.Event()
-    thread = VideosPlayerThread(1, video_names, thread_stop_event)
-    thread.start()
     
+    thread_stop_event = None 
     while True:
         query_str = (raw_input('$ '))
-        print query_str 
         if query_str.find('quit') >= 0 or query_str.find('exit') >= 0:
-            thread_stop_event.set()
+            if thread_stop_event != None: 
+                thread_stop_event.set()
+                play_video_thread.join()
+                play_video_thread = None
             break
+    
         else:
-            print 'processing query_str', query_str
             # match against all videos
+            matched_list = []
+            ranked_list = []
             matched_list = match_indexes(indexes, query_str)
             ranked_list = rank_video(matched_list)
-            event_queue[0].put(ranked_list)
+            
+            if len(ranked_list) == 0:
+                cv2.destroyAllWindows()
+            #event_queue[0].put(ranked_list)
+            else:
+                if thread_stop_event != None: 
+                    thread_stop_event.set()
+                    play_video_thread.join()
+                    play_video_thread = None
+                    
+                thread_stop_event = threading.Event()
+                play_video_thread = RankedVideoPlayer(2, ranked_list, thread_stop_event) 
+                play_video_thread.start()
     '''
     threads = []
     
