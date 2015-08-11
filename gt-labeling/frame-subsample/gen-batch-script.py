1dbc4f1cb117fb84e2f292b80eee35cf4f72cef1
Tiffany <yuhan210@gmail.com>
2015-08-11 13:52:41 -0700

    file reorg

--- gt-labeling/frame-subsample/gen-batch-script.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-
-for f in os.listdir(sys.argv[1]):
-    
-    print 'python subsample.py ' + os.path.join(sys.argv[1], f) + ' &'
-
--- gt-labeling/frame-subsample/run-subsample.sh
+++ gt-labeling/frame-subsample/run-subsample.sh
@@ -1,2 +1,3 @@
 
-seq 1220 | xargs -i --max-procs=25 bash -c "python /home/t-yuche/gt-labeling/frame-subsample/subsample.py {}"
+#seq 1260 | xargs -i --max-procs=25 bash -c "python /home/t-yuche/gt-labeling/frame-subsample/subsample.py {}"
+seq 1260 | xargs -i bash -c "python /home/t-yuche/gt-labeling/frame-subsample/subsample.py {}"
