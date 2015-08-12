48af2fae6252ae31985a177e4634a394a6583d21
Tiffany <yuhan210@gmail.com>
2015-08-12 15:33:07 -0700

    minor tweak

--- demo/build_index.py
+++ demo/build_index.py
@@ -4,14 +4,11 @@ CLUSTER_PATH = '/home/t-yuche/clustering/clusterLib'
 sys.path.append(CLUSTER_PATH)
 from cluster import *
 
-
-
 if __name__ == "__main__":
 
     if len(sys.argv) != 4:
         print 'Usage:', sys.argv[0], ' (0: reset 1: append) index_file video_list'
         exit(-1)
-
     
     IS_APPEND = int(sys.argv[1])
     index_file = sys.argv[2]
@@ -31,9 +28,11 @@ if __name__ == "__main__":
             if video_name in index:
                 continue
 
+        
         gt_nodes = load_turker_labels(video_name)
         clusters, linkage_matrix = cluster(gt_nodes)
         index[video_name] = clusters
+
 
     with open(index_file, 'wb') as handle:
         pickle.dump(index, handle)
