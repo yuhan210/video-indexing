4deafe818b9c1ca268578aeb64f437b80aefc54b
Tiffany <yuhan210@gmail.com>
2015-08-25 17:07:25 -0700

    lucky frame eval first commit

---
+++ luckyframe-eval/run.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def uniform_subsample():
+
+if __name__ == "__main__":
+   
+    VIDEO_LIST = '/mnt/videos.txt'
+    videos = open(VIDEO_LIST).read().split()
+
+    for video in videos:
+       print video            
