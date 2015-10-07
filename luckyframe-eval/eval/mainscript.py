47f8e456c342464ab7049dd64d98ba58def67099
Tiffany <yuhan210@gmail.com>
2015-10-06 22:10:31 -0700

    evaluating subsampled videos

--- luckyframe-eval/eval/mainscript.py
+++ luckyframe-eval/eval/mainscript.py
@@ -23,10 +23,9 @@ def load():
         msr_cap[video_name] = _msr_cap_data
         rcnn[video_name] = _rcnn_data
 
-        if vid == 400:
+        if vid == 50:
             break
     return vgg, fei_cap, msr_cap, rcnn
-
 
 
 
@@ -54,6 +53,51 @@ def rank(query, video_tfs):
 
     return ranked_video
 
+
+def get_inrange_fids(start_fid, end_fid, subsampled_frames):
+
+    prev_fid = -1
+    in_range_fids = []
+
+    for f_count, f_name in enumerate(subsampled_frames):
+        fid = int(f_name.split('.')[0])
+
+        if f_count == 0:
+            prev_fid = fid
+                
+        if prev_fid < start_fid and fid > end_fid:
+            in_range_fids += [prev_fid]
+            break
+               
+        if fid >= start_fid and fid <= end_fid:
+            in_range_fids += [fid]
+ 
+        if fid > end_fid:
+            break
+
+    return in_range_fids 
+
+
+
+
+def get_subsampled_tf(video_name, vgg_data, msr_data, rcnn_data, fei_data, in_range_fids):
+    s_vgg_data = []
+    s_fei_data = []
+    s_msr_data = []
+    s_rcnn_data = []
+    
+    for fid in in_range_fids:
+
+        s_vgg_data += filter(lambda x: int(x['img_path'].split('/')[-1].split('.')[0]) == fid , vgg_data)
+        s_fei_data += filter(lambda x: int(x['img_path'].split('/')[-1].split('.')[0]) == fid , fei_data)
+        s_msr_data += filter(lambda x: int(x['img_path'].split('/')[-1].split('.')[0]) == fid , msr_data)
+        s_rcnn_data += filter(lambda x: int(x['image_path'].split('/')[-1].split('.')[0]) == fid, rcnn_data)
+
+    s_tf_list = combine_all_models(video_name, s_vgg_data, s_msr_data, s_rcnn_data, s_fei_data)
+    s_tf = get_combined_tfs(s_tf_list)
+
+    return s_tf
+
 def run(vgg, fei, msr, rcnn):
     
     SERVER_STORAGE_FRAMES = 5 * 30 # 5 sec * 30 fps
@@ -66,48 +110,77 @@ def run(vgg, fei, msr, rcnn):
 
 
     ## Modify ##
-    subsample_folder = './train/log'
+    greedy_folder = '/home/t-yuche/luckyframe-eval/greedy-log'
     ##    
 
     server_storage = []
     for query in queries:
         n_video_tfs = []
-        s_video_tfs = []
-        for video_name in videos:
-        
-            if video_name not in vgg: 
-                continue
+        uni_video_tfs = []
+        greedy_video_tfs = []
 
-            #subsampled_frames = load_subsampled_frames(video_name, subsample_folder)
-            #naive_subsample_frames(all_frames, RETAINED_RATE) 
-            video_len_f = len(vgg[video_name])
+        for vid, video_name in enumerate(videos):
             
+            if video_name not in vgg:
+                continue
+            
+            # load data
+            vgg_data = vgg[video_name]
+            fei_data = fei[video_name]
+            msr_data = msr[video_name]
+            rcnn_data = rcnn[video_name]
+
+            # load greedy subsampled frames
+            greedy_gt_path = os.path.join(greedy_folder, video_name +  '_gtframe.pickle')
+            with open(greedy_gt_path) as gt_fh:
+                selected_frame_obj = pickle.load(gt_fh)
+                greedy_frames = [ str(x) + '.jpg' for x in selected_frame_obj['picked_f']]
+                video_len_f = selected_frame_obj['total_frame']
+                subsampled_rate = selected_frame_obj['picked_rate']
+                 
+            # baseline
+            uniform_frames = naive_subsample_frames(os.listdir(os.path.join('/mnt/frames', video_name)), subsampled_rate) 
+            
+
             # add random delays to each video stream -- starting at random position
             hash_str = video_name + query
             hash_val = int(abs(hash(hash_str)))%10
             video_start_fid = (video_len_f/10) * hash_val  
             video_end_fid = max(video_start_fid + SERVER_STORAGE_FRAMES, video_len_f - 1)
-           
+          
+            ## Non-subsampled video ## 
             # process in-server info for non-subsampled video
-            _vgg_data = filter(lambda x: int(x['img_path'].split('/')[-1].split('.')[0]) >= video_start_fid and int(x['img_path'].split('/')[-1].split('.')[0]) <= video_end_fid, vgg[video_name])
-            _fei_data = filter(lambda x: int(x['img_path'].split('/')[-1].split('.')[0]) >= video_start_fid and int(x['img_path'].split('/')[-1].split('.')[0]) <= video_end_fid, fei[video_name])
-            _msr_data = filter(lambda x: int(x['img_path'].split('/')[-1].split('.')[0]) >= video_start_fid and int(x['img_path'].split('/')[-1].split('.')[0]) <= video_end_fid, msr[video_name])
-            _rcnn_data = filter(lambda x: int(x['image_path'].split('/')[-1].split('.')[0]) >= video_start_fid and int(x['image_path'].split('/')[-1].split('.')[0]) <= video_end_fid, rcnn[video_name])
+            _vgg_data = filter(lambda x: int(x['img_path'].split('/')[-1].split('.')[0]) >= video_start_fid and int(x['img_path'].split('/')[-1].split('.')[0]) <= video_end_fid, vgg_data)
+            _fei_data = filter(lambda x: int(x['img_path'].split('/')[-1].split('.')[0]) >= video_start_fid and int(x['img_path'].split('/')[-1].split('.')[0]) <= video_end_fid, fei_data)
+            _msr_data = filter(lambda x: int(x['img_path'].split('/')[-1].split('.')[0]) >= video_start_fid and int(x['img_path'].split('/')[-1].split('.')[0]) <= video_end_fid, msr_data)
+            _rcnn_data = filter(lambda x: int(x['image_path'].split('/')[-1].split('.')[0]) >= video_start_fid and int(x['image_path'].split('/')[-1].split('.')[0]) <= video_end_fid, rcnn_data)
             
             tf_list = combine_all_models(video_name, _vgg_data, _msr_data, _rcnn_data, _fei_data)
             tf = get_combined_tfs(tf_list)
 
             #print tf
             n_video_tfs += [{'video_name': video_name, 'tf': tf}]
-            
+    
+            ## Baseline -- uniformly subsampled video ## 
             # process in-server info for subsampled video
-            # can only be a subset of the non-subsampled one/the previous one (not in this range)
-                  
+            uni_range_fids = get_inrange_fids(video_start_fid, video_end_fid, uniform_frames)
+            uni_tf = get_subsampled_tf(video_name, vgg_data, msr_data, rcnn_data, fei_data, uni_range_fids)
+            uni_video_tfs += [{'video_name': video_name, 'tf': uni_tf}]
+
+            ## Greedy -- subsampled video ##
+            greedy_range_fids = get_inrange_fids(video_start_fid, video_end_fid, greedy_frames)
+            greedy_tf = get_subsampled_tf(video_name, vgg_data, msr_data, rcnn_data, fei_data, greedy_range_fids)
+            greedy_video_tfs += [{'video_name': video_name, 'tf': greedy_tf}]
+
 
         # ranking for a given query
-        n_video_rank = rank(query, n_video_tfs)      
+        n_video_rank = rank(query, n_video_tfs)
+        uni_video_rank = rank(query, uni_video_tfs) 
+        greedy_video_rank = rank(query, greedy_video_tfs)
+ 
         print n_video_rank                        
-    
+        print uni_video_rank
+        print greedy_video_rank    
 
 #def run(vgg, fei, msr, rcnn):
 #    return 
