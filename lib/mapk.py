448dd4801c006ce7b7155873a187cff2169c92e2
Tiffany <yuhan210@gmail.com>
2015-10-07 17:30:26 -0700

    mean average percision

---
+++ lib/mapk.py
@@ -0,0 +1,75 @@
+import numpy as np
+
+def apk(actual, predicted, k=10):
+    """
+    Computes the average precision at k.
+
+    This function computes the average prescision at k between two lists of
+    items.
+
+    Parameters
+    ----------
+    actual : list
+             A list of elements that are to be predicted (order doesn't matter)
+    predicted : list
+                A list of predicted elements (order does matter)
+    k : int, optional
+        The maximum number of predicted elements
+
+    Returns
+    -------
+    score : double
+            The average precision at k over the input lists
+
+    """
+    if len(predicted)>k:
+        predicted = predicted[:k]
+
+    score = 0.0
+    num_hits = 0.0
+
+    for i,p in enumerate(predicted):
+        if p in actual and p not in predicted[:i]:
+            num_hits += 1.0
+            score += num_hits / (i+1.0)
+
+    if not actual:
+        return 0.0
+
+    return score / min(len(actual), k)
+
+def mapk(actual, predicted, k=10):
+    """
+    Computes the mean average precision at k.
+
+    This function computes the mean average prescision at k between two lists
+    of lists of items.
+
+    Parameters
+    ----------
+    actual : list
+             A list of lists of elements that are to be predicted 
+             (order doesn't matter in the lists)
+    predicted : list
+                A list of lists of predicted elements
+                (order matters in the lists)
+    k : int, optional
+        The maximum number of predicted elements
+
+    Returns
+    -------
+    score : double
+            The mean average precision at k over the input lists
+
+    """
+    return np.mean([apk(a,p,k) for a,p in zip(actual, predicted)])
+
+'''
+print apk(range(1,6), [6,4,7,1,2], 2)
+print apk(range(1,6), [1,1,1,1,1], 5)
+predicted = range(1,21)
+predicted.extend(range(200,600))
+print apk(range(1,100), predicted ,20)
+print mapk([[1,3,4],[1,2,4],[1,3]], [range(1,6),range(1,6),range(1,6)], 3)
+print mapk([[1,3],[1,2,3],[1,2,3]], [range(1,6),[1,1,1],[1,2,1]], 3)
+'''
