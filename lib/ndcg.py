6dcda84eda51b3453b2672f5a0db9b90bdf029b0
Tiffany <yuhan210@gmail.com>
2015-10-08 15:14:25 -0700

    adding score threshold

--- lib/ndcg.py
+++ lib/ndcg.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def get_video_rel(gt_video_rank, method = 0):
+def get_video_rel(gt_video_rank, score_thresh, method = 0):
     """
     method 0: [1. 0.6309, 0.5, 0.4307, ...]
     method 1: reciprocal rank score
@@ -12,20 +12,20 @@ def get_video_rel(gt_video_rank, method = 0):
         video_name = tup[0] 
         score = tup[1]
         if method == 0:
-            if score > 0:
+            if score > score_thresh:
                 video_rel[video_name] = 1/np.log2(tid + 2)
             else:
                 video_rel[video_name] = 0.0
 
         elif method == 1:
-            if score > 0: 
+            if score > score_thresh: 
                 video_rel[video_name] = 1/((tid + 1) * 1.0) 
             else:
                 video_rel[video_name] = 0.0
         
     return video_rel
 
-def dcg(video_rel, test_video_rank):
+def dcg(video_rel, test_video_rank, score_thresh = 0):
     """
     video_rel: mapping from video_name to its score
     test_video_rank: a sorted list of (video_name, score) 
@@ -36,7 +36,7 @@ def dcg(video_rel, test_video_rank):
         video_name = tup[0]
         score = tup[1]
     
-        if score > 0:
+        if score > score_thresh:
             if tid == 0:
                 dcg_score += video_rel[video_name]
             else:
