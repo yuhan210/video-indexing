b218d4c02c16641f9cd3b94c04605c85d2901f18
Tiffany <yuhan210@gmail.com>
2015-10-13 13:11:19 -0700

    minor changes

--- luckyframe-eval/eval/mainscript.py
+++ luckyframe-eval/eval/mainscript.py
@@ -14,6 +14,94 @@ except:
 
 COSSIM_THRESH = 0.05
 
+def combine_all_models(video_name, _vgg_data, _msr_data, _rcnn_data, _fei_data):
+
+    stop_words = get_stopwords()
+    wptospd = word_pref_to_stopword_pref_dict()
+    convert_dict = convert_to_equal_word()
+
+    tf_list = []
+    assert(len(_vgg_data) == len(_msr_data))
+    assert(len(_rcnn_data) == len(_fei_data))
+    assert(len(_vgg_data) == len(_fei_data))
+
+    for fid in xrange(len(_vgg_data)):
+
+        rcnn_data = _rcnn_data[fid]
+        vgg_data = _vgg_data[fid]
+        msr_data = _msr_data[fid]
+        fei_data = _fei_data[fid]
+   
+        frame_name = rcnn_data['image_path'].split('/')[-1]
+        assert(rcnn_data['image_path'] == vgg_data['img_path'])
+        assert(rcnn_data['image_path'] == msr_data['img_path'])
+        assert(rcnn_data['image_path'] == fei_data['img_path'])
+
+        # combine words
+        rcnn_ws = []
+        if len(rcnn_data) > 0:
+            for rcnn_idx, word in enumerate(rcnn_data['pred']['text']):
+                ## the confidence is higher than 10^(-3) and is not background
+                if rcnn_data['pred']['conf'][rcnn_idx] > 0.0005 and word not in stop_words:
+                    rcnn_ws += [word]
+        vgg_ws = []
+        if len(vgg_data) > 0:
+            for vgg_idx, w in enumerate(vgg_data['pred']['text']):
+                w = wptospd[w]
+                if w in convert_dict:
+                    w = convert_dict[w]
+                prob = vgg_data['pred']['conf'][vgg_idx]
+            
+                if w not in stop_words:
+                    vgg_ws += [w]
+    
+        fei_ws = [] 
+        if len(fei_data) > 0:
+            str_list = fei_data['candidate']['text']
+            for s in str_list:
+                for w in s.split(' '):
+                    w = inflection.singularize(w)
+                    if w not in stop_words and w not in fei_ws:
+                        fei_ws += [w]         
+
+        msr_ws = [] 
+        if len(msr_data) > 0:
+            for msr_idx, w in enumerate(msr_data['words']['text']):
+                w = inflection.singularize(w)
+                prob = msr_data['words']['prob'][msr_idx]
+                if w not in stop_words:
+                    msr_ws += [w]
+
+        words = {}
+        for w in rcnn_ws:
+            if w not in words:
+                words[w] = 1
+            else:
+                words[w] += 1
+        for w in vgg_ws:
+            if w not in words:
+                words[w] = 1
+            else:
+                words[w] += 1
+        
+        for w in fei_ws:
+            if w not in words:
+                words[w] = 1
+            else:
+                words[w] += 1
+    
+        for w_idx, w in enumerate(msr_ws):
+            if w not in words:
+                words[w] = 1
+            else:
+                words[w] += 1
+
+        if '' in words:
+            words.pop('', None)
+
+        tf_list += [{'frame_name': frame_name, 'tf': words}]
+
+    return tf_list
 
 def load():
     
@@ -32,13 +120,13 @@ def load():
         _rcnn_data = load_video_rcnn('/mnt/tags/rcnn-info-all', video_name)
         _turker_data = load_video_processed_turker(video_name)       
  
-        vgg[video_name] = _vgg_data
-        fei_cap[video_name] = _fei_caption_data
-        msr_cap[video_name] = _msr_cap_data
-        rcnn[video_name] = _rcnn_data
-        turker_labels[video_name] = _turker_data
+        vgg[video_name] = _vgg_data[0]
+        fei_cap[video_name] = _fei_caption_data[0]
+        msr_cap[video_name] = _msr_cap_data[0]
+        rcnn[video_name] = _rcnn_data[0]
+        turker_labels[video_name] = _turker_data[0]
 
-        if vid == 20:
+        if vid == 30:
             break
     return vgg, fei_cap, msr_cap, rcnn, turker_labels
 
@@ -213,6 +301,7 @@ def plot_rank_debug(gt_rank, gt_tfs, test_tfs, video_rank, query_str, test_schem
 
 def run(vgg, fei, msr, rcnn, turker_labels):
     SERVER_STORAGE_FRAMES = 5 * 30 # 5 sec * 30 fps
+    BASELINE_SAMPLERATE = 0.02
 
     QUERY_LIST = './query.txt'
     queries = open(QUERY_LIST).read().split()
@@ -221,6 +310,169 @@ def run(vgg, fei, msr, rcnn, turker_labels):
     videos = open(VIDEO_LIST).read().split()
 
 
+    ## Modify ##
+    greedy_folder = '/home/t-yuche/luckyframe-eval/greedy-log'
+    ##    
+    query_dcgs = {'uniform': [], 'greedy': []}
+    server_storage = []
+    for query_str in queries:
+        query = query_str.split('-')[0]
+
+        n_video_tfs = []
+        uni_video_tfs = []
+        greedy_video_tfs = []
+        turker_video_tfs = []
+        sample_rates = []
+
+        for vid, video_name in enumerate(videos):
+        
+            if video_name not in vgg or video_name not in turker_labels or video_name == "100_mile_wilderness_sled_dog_race_Qv4I_MDX7ws" or video_name == "70000_subscriber_thank_you_2bg_real_life_basketball_ep_5__game_of_horse_Qft3fOZjBgU":
+                continue
+            
+            # load data
+            vgg_data = vgg[video_name]
+            fei_data = fei[video_name]
+            msr_data = msr[video_name]
+            rcnn_data = rcnn[video_name]
+            turker_data = turker_labels[video_name]
+
+            # load greedy subsampled frames
+            greedy_gt_path = os.path.join(greedy_folder, video_name +  '_0.4_gtframe.pickle')
+            with open(greedy_gt_path) as gt_fh:
+                selected_frame_obj = pickle.load(gt_fh)
+                greedy_frames = [ str(x) + '.jpg' for x in selected_frame_obj['picked_f']]
+                video_len_f = selected_frame_obj['total_frame']
+                subsampled_rate = selected_frame_obj['picked_rate']
+                sample_rates += [subsampled_rate]
+                #print subsampled_rate
+                    
+            # baseline
+            uniform_frames = naive_subsample_frames(os.listdir(os.path.join('/mnt/frames', video_name)), BASELINE_SAMPLERATE) 
+             
+            # turker frames
+            turker_frames = sorted([x['frame_name'] for x in turker_data], key=lambda x:int(x.split('.')[0]))
+            
+ 
+            # add random delays to each video stream -- starting at random position
+            hash_str = video_name + query_str
+            hash_val = int(abs(hash(hash_str)))%10
+            video_start_fid = int((video_len_f/10.0) * hash_val)
+            video_end_fid = min(video_start_fid + SERVER_STORAGE_FRAMES, video_len_f - 1)
+
+            #print video_name, video_start_fid, video_end_fid
+        
+            '''       
+            ## Turker labeled frames ## 
+            '''
+            '''
+            turker_range_fids = get_inrange_fids(video_start_fid, video_end_fid, turker_frames)
+            turker_tf = get_turker_subsampled_tf(video_name, turker_data, turker_range_fids)
+            turker_video_tfs += [{'video_name': video_name, 'tf': turker_tf}]
+            '''
+            #print turker_frames
+            #print turker_range_fids
+            
+            '''
+            ## Non-subsampled video ## 
+            '''
+            # process in-server info for non-subsampled video
+            _vgg_data = filter(lambda x: int(x['img_path'].split('/')[-1].split('.')[0]) >= video_start_fid and int(x['img_path'].split('/')[-1].split('.')[0]) <= video_end_fid, vgg_data)
+            _fei_data = filter(lambda x: int(x['img_path'].split('/')[-1].split('.')[0]) >= video_start_fid and int(x['img_path'].split('/')[-1].split('.')[0]) <= video_end_fid, fei_data)
+            _msr_data = filter(lambda x: int(x['img_path'].split('/')[-1].split('.')[0]) >= video_start_fid and int(x['img_path'].split('/')[-1].split('.')[0]) <= video_end_fid, msr_data)
+            _rcnn_data = filter(lambda x: int(x['image_path'].split('/')[-1].split('.')[0]) >= video_start_fid and int(x['image_path'].split('/')[-1].split('.')[0]) <= video_end_fid, rcnn_data)
+            
+            tf_list = combine_all_models(video_name, _vgg_data, _msr_data, _rcnn_data, _fei_data)
+            tf = get_combined_tfs(tf_list)
+            n_video_tfs += [{'video_name': video_name, 'tf': tf}]
+            #print n_video_tfs
+              
+            '''
+            ## Baseline -- uniformly subsampled video ## 
+            '''
+            # process in-server info for subsampled video
+            uni_range_fids = get_inrange_fids(video_start_fid, video_end_fid, uniform_frames)
+            uni_tf = get_subsampled_tf(video_name, vgg_data, msr_data, rcnn_data, fei_data, uni_range_fids)
+            uni_video_tfs += [{'video_name': video_name, 'tf': uni_tf}]
+
+            '''
+            ## Greedy -- subsampled video ##
+            '''
+            greedy_range_fids = get_inrange_fids(video_start_fid, video_end_fid, greedy_frames)
+            #print 'greedy frames', greedy_frames
+            #print 'greedy range fids', greedy_range_fids
+            greedy_tf = get_subsampled_tf(video_name, vgg_data, msr_data, rcnn_data, fei_data, greedy_range_fids)
+            greedy_video_tfs += [{'video_name': video_name, 'tf': greedy_tf}]
+            #print greedy_video_tfs
+
+        # ranking for a given query
+        n_video_rank = rank(query, n_video_tfs)
+        uni_video_rank = rank(query, uni_video_tfs) 
+        greedy_video_rank = rank(query, greedy_video_tfs)
+        #print turker_video_tfs
+        #turker_video_rank = rank(query, turker_video_tfs) 
+
+        rn_video_rank, k = reindexing(n_video_rank, n_video_rank, COSSIM_THRESH)
+        runi_video_rank, k  = reindexing(n_video_rank, uni_video_rank, COSSIM_THRESH)
+        rgreedy_video_rank, k = reindexing(n_video_rank, greedy_video_rank, COSSIM_THRESH)
+        #rturker_video_rank = reindexing(n_video_rank, turker_video_rank, COSSIM_THRESH)
+        
+        video_rel = get_video_rel(n_video_rank, COSSIM_THRESH)    
+        gt_dcg = dcg(video_rel, n_video_rank, COSSIM_THRESH)
+
+        #turker_video_rel = get_video_rel(turker_video_rank, COSSIM_THRESH)
+        #turker_dcg = dcg(turker_video_rel, turker_video_rank, COSSIM_THRESH)
+        #print 'gt_dcg', gt_dcg
+ 
+        #print n_video_rank
+        #print rn_video_rank
+        #print rn_video_rank
+        #print '\n--------- Query', query_str, '-------'
+        #print 'Ground-truth'
+        #print n_video_rank 
+        #print 'kendall tau:', kendall_correlation(rn_video_rank, rn_video_rank)
+        #print 'APK:', apk(rn_video_rank, rn_video_rank, k)
+        #print 'dcg:', dcg(video_rel, n_video_rank, COSSIM_THRESH)/gt_dcg
+
+        
+        #print uni_video_rank
+        #print rn_video_rank
+        print '\nUniform'
+        print runi_video_rank
+        #print 'kendall tau:', kendall_correlation(rn_video_rank, runi_video_rank)
+        #print 'APK:', apk(rn_video_rank, runi_video_rank, k)
+        uni_dcg =  dcg(video_rel, uni_video_rank, COSSIM_THRESH)/gt_dcg
+        print 'dcg:', uni_dcg
+        query_dcgs['uniform'] += [uni_dcg]
+
+        #print greedy_video_rank
+        print '\nGreedy'
+        print rgreedy_video_rank
+        print greedy_video_rank
+        #print 'kendall tau:', kendall_correlation(rn_video_rank, rgreedy_video_rank)
+        #print 'APK:', apk(rn_video_rank, rgreedy_video_rank, k)
+        greedy_dcg = dcg(video_rel, greedy_video_rank, COSSIM_THRESH)/gt_dcg
+        print 'dcg:', greedy_dcg
+        query_dcgs['greedy'] += [greedy_dcg]
+        #print 'ave sample rate:', np.mean(sample_rates), np.std(sample_rates, ddof = 1)
+        
+        #plot_rank_debug(n_video_rank, n_video_tfs, uni_video_tfs, uni_video_rank, query_str, test_scheme_name = "uniform")
+        #plot_rank_debug(n_video_rank, n_video_tfs, greedy_video_tfs, greedy_video_rank, query_str, test_scheme_name = "greedy")
+    print '' 
+    print 'Unifrom:', np.mean(query_dcgs['uniform']), np.std(query_dcgs['uniform'])
+    print 'Greedy:', np.mean(query_dcgs['greedy']), np.std(query_dcgs['greedy'])
+
+def run_parallel(vgg, fei, msr, rcnn, turker_labels):
+
+    SERVER_STORAGE_FRAMES = 5 * 30 # 5 sec * 30 fps
+
+    QUERY_LIST = './query.txt'
+    queries = open(QUERY_LIST).read().split()
+    
+    VIDEO_LIST = '/mnt/video_list.txt'
+    videos = open(VIDEO_LIST).read().split()
+
+    
+    
     ## Modify ##
     greedy_folder = '/home/t-yuche/luckyframe-eval/greedy-log'
     ##    
