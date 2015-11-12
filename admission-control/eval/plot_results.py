32e644ff2362ce12b288481e992bd519db3d827e
Tiffany <yuhan210@gmail.com>
2015-11-12 12:11:49 -0800

    parsing logged data and plot results

--- admission-control/eval/plot_results.py
+++ admission-control/eval/plot_results.py
@@ -1,3 +1,7 @@
+import os
+import math
+import pickle
+import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 try:
@@ -5,26 +9,99 @@ try:
 except:
     pass
 
+
+def get_greedy_ave_samplerate(file_name):
+
+    lines = open(file_name).readlines()
+    sample_rates = {}
+
+    for line in lines:
+        line = line.strip()
+        segs = [float(x) for x in line.split()]
+        thresh = segs[0]
+        sample_rate = segs[1]
+        sample_rates[thresh]  = sample_rate
+
+    return sample_rates
+
+def getAllVideosRecall(greedy_thresh, sample_rate, RESULT_FOLDER = '/home/t-yuche/admission-control/eval/greedy-results'):
+
+    VIDEO_LIST = '/mnt/video_list.txt'
+    videos = open(VIDEO_LIST).read().split()
+   
+    greedy_scores = []
+    uniform_scores = [] 
+    for video_name in videos:
+    
+        g, u = getPerVideoRecall(video_name, greedy_thresh, sample_rate)
+        greedy_scores += g 
+        uniform_scores += u
+
+    return greedy_scores, uniform_scores 
+
+def getPerVideoRecall(video_name, greedy_thresh, sample_rate, RESULT_FOLDER= '/home/t-yuche/admission-control/eval/greedy-results' ):
+    greedy_fname = os.path.join(RESULT_FOLDER, video_name + '_greedy_' + str(greedy_thresh) + '_' + str(sample_rate) + '.pickle')
+    uniform_fname = os.path.join(RESULT_FOLDER, video_name + '_uniform_' + str(greedy_thresh) + '_' + str(sample_rate) + '.pickle')
+
+    with open(greedy_fname) as fh:
+        greedy_scores = pickle.load(fh)
+    with open(uniform_fname) as fh:
+        uniform_scores = pickle.load(fh)
+    
+    return greedy_scores, uniform_scores
+
 if __name__ == "__main__":
 
 
-    # read results
-     
-    baseline_rate = [0.02, 0.1, 1]
-    baseline_acc = [0.9, 0.94 ,1]
-    baseline_accerr = [0.088, 0.086 , 0]
+    RESULT_FOLDER = '/home/t-yuche/admission-control/eval/greedy-results' 
+    VIDEO_LIST = '/mnt/video_list.txt'
+    videos = open(VIDEO_LIST).read().split()
+    sample_rates_dict = get_greedy_ave_samplerate('/home/t-yuche/admission-control/tools/greedy_sample_rates')
+    #THRESHS = [0.4, 0.5, 0.6, 0.7, 0.8, 0.9]    
+    THRESHS = [0.8]    
 
-    greedy_rate = [0.0206959443328]
-    greedy_acc = [0.88 ]
-    greedy_rateerr = [ 0.00955137714151]
-    greedy_accerr = [0.1413]
- 
+    uniform_rates = []
+    uniform_acc = []
+    uniform_accerr = []
+
+    greedy_rates = []
+    greedy_acc = []
+    greedy_accerr = []     
+
+    for thresh in THRESHS:
+        g, u = getAllVideosRecall(thresh, sample_rates_dict[thresh])   
+        print 'Greedy:', np.mean(g), np.std(g, ddof = 1) 
+        print 'Uniform:', np.mean(u), np.std(u, ddof = 1)
+
+        uniform_rates += [sample_rates_dict[thresh]]          
+        greedy_rates += [sample_rates_dict[thresh]] 
+        
+        uniform_acc += [np.mean(u)]
+        greedy_acc += [np.mean(g)]
+        
+        
+        uniform_accerr += [np.std(u, ddof = 1)/math.sqrt(len(u))]
+        greedy_accerr += [np.std(g, ddof = 1)/math.sqrt(len(g))]
+
+
+    uniform_rates += [1]
+    greedy_rates += [1]
+    
+    uniform_acc += [1]
+    greedy_acc += [1]
+    
+    uniform_accerr += [0]
+    greedy_accerr += [0]
+
+
     plt.figure()
-    plt.errorbar(baseline_rate, baseline_acc, yerr=baseline_accerr, color = 'b', fmt='--o', label='Uniformly')
+    plt.errorbar(uniform_rates, uniform_acc, yerr=uniform_accerr, color = 'b', fmt = '--o', label = 'Uniform')
     plt.xscale('log')
-    plt.errorbar(greedy_rate, greedy_acc, xerr=greedy_rateerr, yerr=greedy_accerr, color = 'r', fmt='--x', label='Greedy')
+    plt.errorbar(greedy_rates, greedy_acc, yerr=greedy_accerr, color = 'r', fmt = '--x', label = 'Greedy')
     plt.xscale('log')
+    plt.ylim([0, 1])
     plt.xlabel('Subsample rate')
-    plt.ylabel('Averaged ranking accuracy (%)')
+    plt.ylabel('Average accuracy')
     plt.legend()
     plt.show()
+
