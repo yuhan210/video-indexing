0b4e4eb7e97e936a86f8077a582478cb391f2496
Tiffany <yuhan210@gmail.com>
2015-11-12 14:23:46 -0800

    replace mpeg4 codec with libx264 codec

--- visual-hint/create-video.py
+++ visual-hint/create-video.py
@@ -4,16 +4,22 @@ import cv2
 
 if __name__ == "__main__":
 
-    videos = open('/mnt/video_list.txt').read().split()
+    lines = open('/home/t-yuche/visual-hint/video-seg-info/single_obj.txt').readlines()
 
-    for video_name in videos:
+    for line in lines:
+        line =  line.strip()
+        video_name = line.split(',')[0]
+        start_idx = int(line.split(',')[1])
+        end_idx = int(line.split(',')[2])
+        n_obj = int(line.split(',')[4])
+        
+        n_shownf = int(line.split(',')[-1])
+        n_objf = int(line.split(',')[-2])
         fps, w, h = get_video_fps(video_name + '.mp4')
-        start_idx = 0
-        end_idx = 30 * 5
         
         out_video_name = video_name + '_' + str(start_idx) + '_' + str(end_idx) + '.mp4'
 
-        outstr = 'ffmpeg -framerate ' + str(fps) + ' -start_number ' + str(start_idx) + ' -i /mnt/frames/' + video_name + '/%d.jpg -vframes 150 -s 480x360 -vcodec mpeg4 ' + out_video_name 
+        if n_objf/(n_shownf * 1.0) > 0.025 and n_objf > 2 and n_obj > 5:
+            outstr = '~/ffmpeg/ffmpeg -framerate ' + str(fps) + ' -start_number ' + str(start_idx) + ' -i /mnt/frames/' + video_name + '/%d.jpg -vframes 150 -s 560x420 -vcodec libx264 -pix_fmt yuv420p ' + out_video_name 
 
-        print outstr
-        break
+            print outstr
