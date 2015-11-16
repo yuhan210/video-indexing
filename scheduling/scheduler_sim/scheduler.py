36d186fe476d89d5e4d3e40281f0e15d0a96a4c2
Tiffany <yuhan210@gmail.com>
2015-11-16 14:06:25 -0800

    memory uses utilization as well

--- scheduling/scheduler_sim/scheduler.py
+++ scheduling/scheduler_sim/scheduler.py
@@ -50,7 +50,7 @@ class Machine:
         self.MEM_LIM = MACHINE_MEMORY
         self.CPU_LIM = MACHINE_COMPUTE
 
-        self.mem_remaining = MACHINE_MEMORY 
+        self.mem_util = 0 
         self.core = MACHINE_CORE
         self.cpu_util = 0
    
@@ -58,22 +58,23 @@ class Machine:
         self.cpu_util_log = []  
  
     def __str__(self):
-        return 'machine_id:' + str(self.machine_id) + ' mem_remaining (%):' + str((self.mem_remaining* 100)/self.MEM_LIM) + ' cpu_util (%):' + str((self.cpu_util * 100)/self.CPU_LIM)
+        return 'machine_id:' + str(self.machine_id) + ' mem_util (%):' + str((self.mem_util* 100)/self.MEM_LIM) + ' cpu_util (%):' + str((self.cpu_util * 100)/self.CPU_LIM)
 
     def get_status(self):
 
-        m_status = {'mem_remain_byte': self.mem_remaining, 'mem_util_prec': (self.MEM_LIM - self.mem_remaining)/(self.MEM_LIM * 1.0), 'cpu_util_flop': self.cpu_util, 'cpu_util_prec': self.cpu_util/(self.CPU_LIM * 1.0), 'mem_lim': self.MEM_LIM, 'cpu_lim': self.CPU_LIM}
+        m_status = {'mem_util_byte': self.mem_util, 'mem_util_prec': self.mem_util/(self.MEM_LIM * 1.0), 'cpu_util_flop': self.cpu_util, 'cpu_util_prec': self.cpu_util/(self.CPU_LIM * 1.0), 'mem_lim': self.MEM_LIM, 'cpu_lim': self.CPU_LIM}
+
         return m_status
 
     def does_job_fit(self, job):
        
         # TODO: one process per core? 
         # check if adding this job exceeds mem capacity
-        if self.mem_remaining - job.config['mem_req'] < 0 :
+        if self.mem_util + job.config['mem_req'] > self.MEM_LIM :
             return False
 
         # check if adding this job exceeds cpu util
-        if self.cpu_util + job.config['job_util']> self.CPU_LIM:
+        if self.cpu_util + job.config['job_util'] > self.CPU_LIM:
             return False
 
         return True 
@@ -82,7 +83,7 @@ class Machine:
 
         if scheme == 'nsp': 
             if self.does_job_fit(job):
-                self.mem_remaining -= job.config['mem_req'] 
+                self.mem_util += job.config['mem_req'] 
                 # TODO: verify the variable
                 self.cpu_util += job.config['job_util']
                 job.start_ts = cur_ts 
@@ -91,17 +92,21 @@ class Machine:
                 return True
             return False
 
-    def update(self, cur_ts):    
+    def update(self, cur_ts):   
+ 
         finished_jobs = []
+        # holds the index of the jobs that needs to be deleted
         finished_pids = []
         for idx, job in enumerate(self.jobs): 
             if job.end_ts < cur_ts:
+
                 finished_jobs += [job]
+
                 # release memory and decrease cpu util 
-                self.mem_remaining += job.config['mem_req'] 
-                self.cpu_util -= job.get_job_util()
+                self.mem_util -= job.config['mem_req'] 
+                self.cpu_util -= job.config['job_util']
                 finished_pids += [idx]
-                #logger.info('Mach(%d) finished processing Job(%s)', self.machine_id, job)
+
         for idx in reversed(finished_pids):
             del self.jobs[idx] # remove process
 
@@ -138,6 +143,7 @@ class Global:
             return job_config
 
 class Cloud:
+
     def __init__(self, scheme): 
         self.n_machine = N_MACHINE
         self.scheme = scheme
@@ -146,15 +152,31 @@ class Cloud:
         for i in xrange(self.n_machine):
             self.machines[i] = Machine(i)
 
+
+    def get_machine_statues(self):
+
+        machine_statuses = {}
+        for mid in self.machines:
+            machine = self.machines[mid]
+            machine_statuses[mid] = machine.get_status()
+
+        return machine_statuses
+
+    def get_cloud_util(self):
+
+        machine_statuses = self.get_machine_statues()
+        for mid in machine_statuses:          
+            machine_statuses[mid] 
+
     def does_machine_fit(self, machine_status, job):
        
          
-        if machine_status['mem_remain_byte'] - job.config['mem_req'] < 0:
-            #logger.debug('Exceed mem util -- remain mem: %f, job mem req: %f', machine_status['mem_remain_byte'] , job.config['mem_req'])
+        if machine_status['mem_util_byte'] + job.config['mem_req'] > machine_status['mem_lim']:
+            #logger.debug('Exceed mem util -- mem util: %f, job mem req: %f', machine_status['mem_util_byte'] , job.config['mem_req'])
             return False
 
         if machine_status['cpu_util_flop'] + job.config['job_util']  > machine_status['cpu_lim']:
-            #logger.debug('Exceed cpu util -- Cur CPU util: %f, job util: %f', machine_status['cpu_util_flop'],  job.config['job_util'])
+            #logger.debug('Exceed cpu util -- CPU util: %f, job util: %f', machine_status['cpu_util_flop'],  job.config['job_util'])
             return False
 
         return True
@@ -166,10 +188,9 @@ class Cloud:
 
             start_jobs = [] 
             for jid, job in enumerate(job_queue):
-                machine_statuses = {}
-                for mid in self.machines:
-                    machine = self.machines[mid]
-                    machine_statuses[mid] = machine.get_status()
+
+                machine_statuses = self.get_machine_statues()
+
                 for mid in machine_statuses:
                     if self.does_machine_fit(machine_statuses[mid], job):
                         success = self.machines[mid].run(self.scheme, job, cur_ts)
@@ -251,6 +272,7 @@ def run_scheme(scheme):
     
     job_queue = []
     stream_process_log = dict([(key, {}) for key in range(len(streams))])
+    #cloud_util_log  
     #stream_process_log = dict.fromkeys(range(len(streams))) # a dict (key: sid) of dict (key: start_ts, value: end_ts)
     print 'Simulation starts'
 
@@ -284,18 +306,22 @@ def run_scheme(scheme):
                 job_str += str(job) + '\n'
             logger.info('%d -- job queue: %s', cur_ts, job_str)
             
-        # cloud processes job queue, update machine occupancy
+        # cloud updates machine occupancy
         finished_jobs = cloud.update(cur_ts)
-        
 
         for fj in finished_jobs:
             #print fj
             stream_process_log[fj.stream_id][fj.emit_ts] = (fj.start_ts, fj.end_ts)
 
+
         prev_q_size = len(job_queue)
+        
+        # cloud schedules job in the queue
         job_queue = cloud.schedule(job_queue, cur_ts)
- 
+
+        machine_statuses = cloud.get_machine_statues() 
         cur_ts += 1 
+
     print stream_process_log
         
 if __name__ == "__main__":
