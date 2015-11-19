b57aa5493cdb0df55b63f6d0ebd50d38b8d5fc1d
Tiffany <yuhan210@gmail.com>
2015-11-18 16:22:15 -0800

    svm model training

---
+++ admission-control/train/svm/code/train.py
@@ -0,0 +1,30 @@
+import sys
+sys.path.append('../liblinear/python')
+from svmutil import *
+import os
+
+DATA_FOLDER = 'data'
+MODEL_FOLDER = 'model'
+
+def train_model(train_data_path, model_output_path, PARAM):
+    y, x = svm_read_problem(train_data_path)
+    m = svm_train(y, x, '-t ' + PARAM['-t'] + ' -c ' + PARAM['-c']) 
+
+    svm_save_model(model_output_path, m)
+
+if __name__ == "__main__":
+
+    PARAM = {'-t': '0',  '-c': '10'}
+    
+    for train_data_path in os.listdir(DATA_FOLDER):
+        #svm_train_0.4_8733_2.data
+        segs = train_data_path.split('_')
+        PERCENT_OF_POS_SAMPLES = segs[2]
+        N_POS_SAMPLES = segs[3]
+        P_TO_N_RATIO = segs[4]
+        
+        model_output_path = os.path.join(MODEL_FOLDER, train_data_path.split('.')[0] + '_' + PARAM['-t'] + '_' + PARAM['-c'] + '.model')
+         
+ 
+        train_model(os.path.join(DATA_FOLDER, train_data_path), model_output_path, PARAM)
+
