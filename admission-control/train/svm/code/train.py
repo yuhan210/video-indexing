5f9c0d53e5e2fe0caae636dea140683f3703f4c1
Tiffany <yuhan210@gmail.com>
2015-11-18 16:26:25 -0800

    naming bug

--- admission-control/train/svm/code/train.py
+++ admission-control/train/svm/code/train.py
@@ -19,12 +19,12 @@ if __name__ == "__main__":
     for train_data_path in os.listdir(DATA_FOLDER):
         #svm_train_0.4_8733_2.data
         print 'Training ', train_data_path
-        segs = train_data_path.split('_')
-        PERCENT_OF_POS_SAMPLES = segs[2]
-        N_POS_SAMPLES = segs[3]
-        P_TO_N_RATIO = segs[4]
+        #segs = train_data_path.split('_')
+        #PERCENT_OF_POS_SAMPLES = segs[2]
+        #N_POS_SAMPLES = segs[3]
+        #P_TO_N_RATIO = segs[4]
         
-        model_output_path = os.path.join(MODEL_FOLDER, train_data_path.split('.')[0] + '_' + PARAM['-t'] + '_' + PARAM['-c'] + '.model')
+        model_output_path = os.path.join(MODEL_FOLDER, train_data_path[:-5] + '_' + PARAM['-t'] + '_' + PARAM['-c'] + '.model')
          
  
         train_model(os.path.join(DATA_FOLDER, train_data_path), model_output_path, PARAM)
