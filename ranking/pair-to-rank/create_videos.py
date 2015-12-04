fa87eecfc644e4ae060dac4af854b97a9213b6e2
Tiffany <yuhan210@gmail.com>
2015-12-03 20:55:56 -0800

    create_videos script

---
+++ ranking/pair-to-rank/create_videos.py
@@ -0,0 +1,25 @@
+import pickle
+import tarfile
+import itertools
+import os
+from utils import *
+
+SERVER_WINDOW_SIZE = 30 * 5
+for i in xrange(6, 20): 
+
+    for query in open('../gen_rank_data/single_query').read().split():
+        vs = pickle.load(open('../gen_rank_data/label_videos/' + query + '_' + str(i) + '.pickle'))
+        #pairs = list(itertools.combinations(vs,2))
+        for v in vs:
+            print v
+        #for pair in pairs:
+            video = v['video_name'] + '_' + str(v['start_fid']) + '_' + str(v['end_fid']) + '.mp4'
+            #print video_a + '&video_name_b=' + video_b
+            out_video_path = os.path.join('/mnt/video-segs', video)
+            fps, w, h = get_video_fps(v['video_name'] + '.mp4')
+            if os.path.exists(out_video_path):
+                continue
+            outstr = '~/ffmpeg/ffmpeg -framerate ' + str(fps) + ' -start_number ' + str(v['start_fid']) + ' -i /mnt/frames/' + v['video_name'] + '/%d.jpg -vframes ' + str(SERVER_WINDOW_SIZE) +' -s 560x420 -vcodec libx264 -pix_fmt yuv420p -n ' + out_video_path
+            print outstr 
+            
+
