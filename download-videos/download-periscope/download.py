1ae729073ef1c7ff25e7e83856a519943c0f1408
Tiffany <yuhan210@gmail.com>
2015-12-09 15:49:07 -0800

    downloading periscope videos

---
+++ download-videos/download-periscope/download.py
@@ -0,0 +1,79 @@
+import youtube_dl
+import socket
+import urllib2
+import urllib
+import requests
+from lxml import html
+import time
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+ydl_opts = {
+    'format': 'bestvideo/best',
+    'sanitize_filename': True,
+    'writeinfojson': True,
+    'logger': MyLogger(),
+    'progress_hooks': [my_hook],
+}
+
+def download_video(urls):
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        for url in urls:
+            print url
+            try:
+                ydl.download([url])
+
+            except:
+                print 'Download Error'
+                continue
+
+def parse_video_archive(url):
+
+    watch_urls = []
+    try:
+        html_doc = urllib2.urlopen(url).readlines()
+
+    except urllib2.URLError, e:
+        # For Python 2.7
+        print 'URLError %r' % e
+        return
+
+    except socket.timeout, e:
+        # For Python 2.7
+        print 'Timeout %r' % e
+        return
+
+    urls = []
+    for idx, line in enumerate(html_doc):
+        line = line.strip()
+        if line.find('<span class="label label-danger">Off air!</span>') >= 0:
+            # get url
+            url_string = html_doc[idx + 3]
+            url = url_string[url_string.index('https://'): url_string.index("',")]
+            urls += [url]
+    return urls
+
+
+def start_download():
+    urls = parse_video_archive('http://onperiscope.com/')
+    download_video(urls) 
+
+if __name__ == "__main__":
+
+    while True:
+        start_download()
+        time.sleep(3 * 60)
+    
