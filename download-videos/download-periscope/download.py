f6c12dde3ced207f3d22a2e3c27f45ea98c87a9a
Tiffany <yuhan210@gmail.com>
2015-12-19 12:18:09 -0800

    download periscope videos

--- download-videos/download-periscope/download.py
+++ download-videos/download-periscope/download.py
@@ -4,11 +4,14 @@ import urllib2
 import urllib
 import requests
 from lxml import html
-import time
 
+import time
+import os
 class MyLogger(object):
     def debug(self, msg):
+        print (msg)
         pass
+
 
     def warning(self, msg):
         pass
@@ -25,17 +28,45 @@ ydl_opts = {
     'format': 'bestvideo/best',
     'sanitize_filename': True,
     'writeinfojson': True,
-    'logger': MyLogger(),
-    'progress_hooks': [my_hook],
+    'verbose': True
+    #'logger': MyLogger()
+    #'progress_hooks': [my_hook],
 }
+
+def replace_extension(filename, ext, expected_real_ext=None):
+    name, real_ext = os.path.splitext(filename)
+    return '{0}.{1}'.format(
+        name if not expected_real_ext or real_ext[1:] == expected_real_ext else filename,
+        ext)
+
+def get_metadata(url, filename):
+
+    watch_urls = []
+    try:
+        html_doc = urllib2.urlopen(url).readlines()
+
+    except urllib2.URLError, e:
+        # For Python 2.7
+        print 'URLError %r' % e
+        return
+
+    except socket.timeout, e:
+        # For Python 2.7
+        print 'Timeout %r' % e
+        return
+
+    with open(filename, 'w') as fh:
+        for line in html_doc:
+            fh.write(line)
+         
 
 def download_video(urls):
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         for url in urls:
-            print url
             try:
-                ydl.download([url])
-
+                ret, ress = ydl.download([url])
+                metadata_fname = ress[0]['title'] + '-' + ress[0]['id'] + '.metadata'
+                get_metadata(url, metadata_fname)
             except:
                 print 'Download Error'
                 continue
@@ -74,6 +105,8 @@ def start_download():
 if __name__ == "__main__":
 
     while True:
+        if len(os.listdir('./')) >= 6000:
+            break
         start_download()
-        time.sleep(3 * 60)
+        time.sleep(2 * 60)
     
