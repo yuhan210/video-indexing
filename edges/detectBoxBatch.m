91b92515d9449d7cc4949c8ebe458c534d1e84f5
Tiffany <yuhan210@gmail.com>
2015-12-19 12:19:16 -0800

    edge scripts

--- edges/detectBoxBatch.m
+++ edges/detectBoxBatch.m
@@ -11,7 +11,7 @@ opts.beta  = .75;     % nms threshold for object proposals
 opts.minScore = .01;  % min score of boxes to detect
 opts.maxBoxes = 500;  % max number of boxes to detect
 
-fh = fopen('/mnt/video_list.txt');
+fh = fopen('/home/t-yuche/car2cloud/video_list');
 videos = textscan(fh, '%s', 'Delimiter', '\n');
 videos = videos{1};
 
@@ -21,9 +21,9 @@ parfor vid = 1: length(videos)
     
     video_name = videos{vid};
     disp(video_name)
-    frame_folder = strcat('/mnt/frames/', video_name);
+    frame_folder = strcat('/home/t-yuche/car2cloud/frames/', video_name);
     
-    output_folder = strcat('/mnt/tags/edgebox-all/', video_name, '/');
+    output_folder = strcat('/home/t-yuche/car2cloud/bbox/', video_name, '/');
     if (exist(output_folder, 'dir') == 0), mkdir(output_folder); end
     
     dir_data = dir(frame_folder);
@@ -31,16 +31,18 @@ parfor vid = 1: length(videos)
     for i = 3:length(dir_data)
         frame_name = dir_data(i).name;
         frame_path = strcat(frame_folder, '/', frame_name);
-        %disp(frame_path)
+        disp(frame_path)
+        
+        outfile_path = strcat('/home/t-yuche/car2cloud/bbox/', video_name, '/' , frame_name(1:length(frame_name)-4), '.bbx');
+        if (exist(outfile_path, 'file')), continue; end
         
         % detect bbx
         I = imread(frame_path);
-        tic, bbs=edgeBoxes(I,model,opts); toc
-        disp(toc)
+        tic, bbs=edgeBoxes(I,model,opts); toc;
+        %disp(toc)
                 
         
         %% write bbx into a file        
-        outfile_path = strcat('/mnt/tags/edgebox-all/', video_name, '/' , frame_name(1:length(frame_name)-4), '.bbx');
         fid=fopen(outfile_path,'w'); assert(fid>0);        
         
         [th, tw] = size(bbs);
